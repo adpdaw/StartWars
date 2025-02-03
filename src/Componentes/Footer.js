2fb3a8b9d87ddc959fc13bb554c34a2dbaa3e14a
adiezp <Alfredo.Paya@gft.com>
2025-02-03 16:25:45 +0100

    Added rel="noreferrer"

--- src/Componentes/Footer.js
+++ src/Componentes/Footer.js
@@ -26,7 +26,7 @@ function Footer() {
         <p>
           &copy; <span id="date">{year}</span>
           <span className="footer-logo">My sitio Web</span> Hecho con 💛
-          <a href="https://adp-profile.netlify.app" target="_blank">
+          <a href="https://adp-profile.netlify.app" target="_blank" rel="noreferrer">
             Alfredo Díez
           </a>
         </p>
